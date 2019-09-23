a577e864da9cfd50837c62b897626210015caa8f
Norbert Korom <norbertkorom@gmail.com>
2019-09-23 19:31:06 +0100

    play bootstrap and jquery 1

--- js/my_jQueryFunctions.js
+++ js/my_jQueryFunctions.js
@@ -1,7 +1,15 @@
 
+
 $("#hideButton").click(function(){
   $("p").hide();
+  $("#hideButton").attr("disabled", true);
+  $("#showButton").attr("disabled", false);
 });
+
+$("#showButton").attr("disabled", true);
 $("#showButton").click(function(){
-  $("p").show();
+  $("#firstP").show();
+  $("#secP").show();
+  $("#hideButton").attr("disabled", false);
+  $("#showButton").attr("disabled", true);
 });
